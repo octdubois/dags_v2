4d802438b2c1ba4dabda124bec22816c0c4e2952
octdubois <41754035+octdubois@users.noreply.github.com>
2021-09-29 13:26:14 -0400

    Add files via upload

---
+++ py_infl_dag.py
@@ -0,0 +1,38 @@
+from datetime import timedelta,datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+import sys,os
+sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
+
+from py_infl_etl import run_py_infl_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 9, 12),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    
+}
+
+dag = DAG(
+    'spotify_dag',
+    default_args=default_args,
+    description='Our first DAG with ETL process!',
+    schedule_interval=timedelta(days=1),
+)
+
+def just_a_function():
+    print("I'm going to show you something :)")
+
+run_etl = PythonOperator(
+    task_id='whole_spotify_etl',
+    python_callable=run_py_infl_etl,
+    dag=dag,
+)
+
+run_etl
---
+++ py_infl_etl.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+import pandas as pd
+#import matplotlib.pyplot as plt
+from influxdb import InfluxDBClient
+
+def run_py_infl_etl():
+	query = 'SELECT * FROM "test_temp" WHERE time >= now() - 1d ; '
+	query1 = 'SELECT * FROM "test_t_m" WHERE time >= now() - 1d ; '
+	query2 = 'SELECT * FROM "test_t_M" WHERE time >= now() - 1d ; '
+	query3 = 'SELECT * FROM "test_humi" WHERE time >= now() - 1d ; '
+	
+	client = InfluxDBClient('localhost',8086,database='ESP32_DHT11_SHCU_TSL45')
+
+	result = client.query(query)
+	result1 = client.query(query1)
+	result2 = client.query(query2)
+	result3 = client.query(query3)
+	
+
+	#print("Result: {0}".format(result))
+	points = result.get_points()
+	points1 = result1.get_points()
+	points2 = result2.get_points()
+	points3 = result3.get_points()
+	
+
+
+
+	dfs = pd.DataFrame(points)
+	dfs['time'] = pd.to_datetime(dfs['time'], errors='coerce')
+	dfs.set_index('time', inplace = True)
+	dfs=dfs.resample('30s').mean()
+
+
+	dfs1 = pd.DataFrame(points1)
+	dfs1['time'] = pd.to_datetime(dfs1['time'])
+	dfs1.set_index('time', inplace = True)
+	dfs1=dfs1.resample('30s').mean()
+
+	dfs2 = pd.DataFrame(points2)
+	dfs2['time'] = pd.to_datetime(dfs2['time'], errors='coerce')
+	dfs2.set_index('time', inplace = True)
+	dfs2=dfs2.resample('30s').mean()
+
+
+	dfs3 = pd.DataFrame(points3)
+	dfs3['time'] = pd.to_datetime(dfs3['time'], errors='coerce')
+	dfs3.set_index('time', inplace = True)
+	dfs3=dfs3.resample('30s').mean()
+
+	
+
+
+
+	dfs=dfs.merge(dfs1, how='inner', on='time').merge(dfs2, how='inner', on='time').merge(dfs3, how='inner', on='time')
+	dfs.set_axis(['Sensor(1)','Sensor(2)','Sensor(3)','Sensor(4)'], axis=1)
+	dfs=dfs.fillna(method='ffill')	
+	print(dfs)
+	
+	date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+
+	#ax=dfs['Sensor(1)'].plot()
+	#ax=dfs['Sensor(2)'].plot()
+
+
+
+	#plt.legend(loc='upper left')
+	#ax.set_ylabel("Soil moisture")
+
+	dfs.to_csv("csv/proto"+date+".csv")  
